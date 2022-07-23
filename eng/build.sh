dcd51545cd15ff7b87b8727d95af794fc54ccd1e
kevinransom <codecutter@hotmail.com>
2022-07-23 02:19:08 -0700

    Build

--- eng/build.sh
+++ eng/build.sh
@@ -262,7 +262,7 @@ function BuildSolution {
     fi
     if [ ! -f "$bootstrap_dir/fsc.exe" ]; then
       BuildMessage="Error building bootstrap"
-      MSBuild "$repo_root/proto.proj" \
+      MSBuild "$repo_root/proto.sln" \
         /restore \
         /p:Configuration=$bootstrap_config
 
